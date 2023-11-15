37a1da473b404a4adfd7998c964825d6d2c8b677
Dan Tramte <datramt@gmail.com>
2023-11-15 16:52:32 -0500

    format logic implemented

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,33 @@
-//tsc --init
-//tsc --watch
-//node --watch dist/index.js
+interface Payment {
+    id: number
+    amount: number
+    to: string
+    notes: string
+}
 
-console.log('test')
+type PaymentColumns = ('id' | 'amount' | 'to' | 'notes')[]
+
+class CSVWriter {
+    constructor(private columns: PaymentColumns) {
+        this.csv = this.columns.join(',') + '\n'
+    }
+
+    private csv: string
+
+    addRows(values: Payment[]): void {
+        let rows = values.map((v) => this.formatRow(v))
+        this.csv += rows.join('\n')
+        console.log(this.csv)
+    }
+
+    private formatRow(p: Payment): string {
+        return this.columns.map((col) => p[col]).join(',')
+    }
+}
+
+const writer = new CSVWriter(['id', 'amount', 'to', 'notes'])
+
+writer.addRows([
+    { id: 1, amount: 50, to: 'yoshi', notes: 'for design work' },
+    { id: 2, amount: 40, to: 'mario', notes: 'for pizza'}  
+]) 
